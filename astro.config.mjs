8c5dc5767c5c3cdc56c55e51a1cab8eb241b5ee4
alfonsovillanueva <alfonso@villanuevagarcia.com>
2025-01-21 23:45:05 -0600

    Add - Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+
+import cloudflare from '@astrojs/cloudflare';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+
+  //   output: 'server',
+  // Por default, Astro esta en output: "hybrid" asi que no es necesario ponerlo, salvo que quieras usar el servidor como primer instancia
+  //   adapter: node({
+  //     mode: 'standalone',
+  //   }),
   integrations: [mdx(), sitemap()],
-//   output: 'server',
-// Por default, Astro esta en output: "hybrid" asi que no es necesario ponerlo, salvo que quieras usar el servidor como primer instancia
-  adapter: node({
-    mode: 'standalone',
-  }),
+
+  adapter: cloudflare(),
 });
